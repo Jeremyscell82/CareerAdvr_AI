a0721aa0a0f3b21e8a7e1192c1ae7c8e20fbec02
Jeremy Lloyd <Jeremyscell82@gmail.com>
2024-04-17 11:54:30 -0600

    Changed min build #

--- app/src/main/java/com/lloydsbyte/careeradvr_ai/analytics/MixPanelConstants.kt
+++ app/src/main/java/com/lloydsbyte/careeradvr_ai/analytics/MixPanelConstants.kt
@@ -4,29 +4,6 @@ open class MixPanelConstants {
     companion object {
         const val mixpanelId = "b3b9f37167a910fb79a29464b71a1094"
 
-        val MIX_USAGE = "app_usage"
-        val MIX_ERROR = "app_error"
-
-        const val EVENT_AD_CLICKED = "ad_clicked"
-        const val EVENT_AD_FAILED = "ad_failed"
-
-        const val EVEN_SETTINGS = "Settings"
-        const val VALUE_GPT_MODEL = "Gpt Model"
-        const val VALUE_TEMP = "Gpt Temp"
-
-
-        const val EVENT_TOKEN = "Tokens"
-        const val VALUE_CONVO = "Conversation Tokens"
-        const val VALUE_QUESTIONS = "Questions Asked"
-
-        const val EVENT_CHAT = "Chat Usage"
-        const val VALUE_PRO = "Profession Used"
-        const val VALUE_AMA = "AMA Used"
-
-        const val EVENT_CHAT_RATING = "Chat Rating"
-        const val VALUE_AMA_RATING = "Ama Rating"
-        const val VALUE_PRO_RATING = "Pro Rating"
-
         const val EVENT_ERROR = "Error"
         const val VALUE_ERROR_CHAT = "Gpt Error"
 
